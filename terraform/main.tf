a9afed3fa21d0b3959684908bb53ce40ce79adc5
NaoDekoNeko <ademarx17@gmail.com>
2026-08-07 18:36:47 -0500

    fix(infra): switch node pool to Standard_D2as_v6
    Standard_B2s is not in this subscription's quota for eastus2.

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "system"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_D2as_v6"
   }
 
   identity {
