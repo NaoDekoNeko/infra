e65d247b548f07694990fbf601b9ac48141bd48a
NaoDekoNeko <ademarx17@gmail.com>
2026-08-07 18:37:16 -0500

    feat(infra): provision Azure DNS zone for naodeko.site

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,13 @@ variable "acr_name" {
   type    = string
   default = "acrmibancochallenge" # único global, solo [a-z0-9], 5-50 chars
 }
+
+variable "dns_zone_name" {
+  type    = string
+  default = "naodeko.site"
+}
+
+variable "ingress_public_ip" {
+  type    = string
+  default = "57.166.193.16" # IP del LoadBalancer del F5 NIC (ingress-nginx/nginx-ingress-controller)
+}
